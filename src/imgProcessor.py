b588876c9ba4c69fd41058189bb5f5f0cc9b649a
mortarsynth <denis.uzhva@yahoo.com>
2020-05-11 20:55:25 +0300

    added the waifu2x API processor

--- src/imgProcessor.py
+++ src/imgProcessor.py
@@ -3,6 +3,8 @@
 
 
 import io
+import requests
+from os import environ
 import numpy as np
 from PIL import Image
 from PIL import ImageEnhance
@@ -12,13 +14,13 @@ from PIL import ImageOps
 
 ## Processor commands (options)
 
-proc_commands = ['/neg', '/ctr', '/bgt', '/sat', '/shp']
+proc_cmds = ['/neg', '/ctr', '/bgt', '/sat', '/shp', '/w2x']
 
 
 ## Return the processor commands
 
 def get_proc_cmds():
-    return proc_commands
+    return proc_cmds
   
 
 ## Process an image (in bytes) according to a desired command
@@ -43,6 +45,9 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
         if not errors:
             cmd_arg = float(cmd_args[0])
             img_new = proc_ctr(img, cmd_arg)
+            img_stream = io.BytesIO()
+            img_new.save(img_stream, 'JPEG')
+            img_stream = img_stream.getvalue()
         else:
             img_new = img
             log = 'Invalid arguments...'
@@ -58,6 +63,9 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
         if not errors:
             cmd_arg = float(cmd_args[0])
             img_new = proc_bgt(img, cmd_arg)
+            img_stream = io.BytesIO()
+            img_new.save(img_stream, 'JPEG')
+            img_stream = img_stream.getvalue()
         else:
             img_new = img
             log = 'Invalid arguments..'
@@ -73,6 +81,9 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
         if not errors:
             cmd_arg = float(cmd_args[0])
             img_new = proc_sat(img, cmd_arg)
+            img_stream = io.BytesIO()
+            img_new.save(img_stream, 'JPEG')
+            img_stream = img_stream.getvalue()
         else:
             img_new = img
             log = 'Invalid arguments....'
@@ -88,15 +99,20 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
         if not errors:
             cmd_arg = float(cmd_args[0])
             img_new = proc_shp(img, cmd_arg)
+            img_stream = io.BytesIO()
+            img_new.save(img_stream, 'JPEG')
+            img_stream = img_stream.getvalue()
         else:
             img_new = img
             log = 'Invalid arguments.'
+    
+    elif cmd_name == '/w2x':
+        img_stream = img2x(img_byte)
+    
     else:
         img_new = img
     
-    img_stream = io.BytesIO()
-    img_new.save(img_stream, 'JPEG')
-    img_stream = img_stream.getvalue()
+    
     return img_stream, errors, log
   
 
@@ -126,3 +142,21 @@ def proc_sat(img, factor):
 def proc_shp(img, factor):
     img = ImageEnhance.Sharpness(img).enhance(factor)
     return img
+  
+
+## Enlarger
+
+# enlarge an image (double its size) using the Waifu2x API
+def img2x(img_byte):
+    img_stream = io.BytesIO(img_byte)
+    r = requests.post(
+        "https://api.deepai.org/api/waifu2x",
+        files={
+            'image': img_byte,
+        },
+        headers={'api-key': environ['DEEPAI_API_KEY']}
+    )
+    print(r.json())
+    img_new = requests.get(r.json().get('output_url')).content
+    return img_new
+
