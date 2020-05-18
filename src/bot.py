14c4f61fa41ea74a73b8298b241395c84db1ea08
mortarsynth <denis.uzhva@yahoo.com>
2020-05-18 18:22:04 +0300

    more deepai processors

--- src/bot.py
+++ src/bot.py
@@ -10,17 +10,21 @@ from os import environ
 
 
 ## Setup bot with Telegram token from .env
+
 bot = telebot.TeleBot(environ['TELEGRAM_TOKEN'])
 
 
 ## Basic handler triggered with the /start command
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'hi there')
     
     
 ## Image processing handler
+
 proc_cmds = iproc.get_proc_cmds()
+
 @bot.message_handler(content_types=['text', 'photo'])
 def process_photo(message):
   
@@ -45,7 +49,7 @@ def process_photo(message):
     elif msg_img and not msg_words[0] in proc_cmds:
         bot.send_message(chat_id, 'Invalid command .__.')
     
-    # process if all is ok
+    # launch the processor if all is ok
     elif msg_img and msg_words[0] in proc_cmds:
         proc_cmd = msg_words[0]
         proc_args = []
@@ -54,13 +58,14 @@ def process_photo(message):
         file_id = msg_img[-1].file_id
         img_info = bot.get_file(file_id)
         img_byte = bot.download_file(img_info.file_path)
-        img_byte_new, errors, log = iproc.process_byte(img_byte, proc_cmd, proc_args)
-        if errors:
+        img_byte_new, log_only, log = iproc.process_byte(img_byte, proc_cmd, proc_args)
+        if log_only:
             bot.send_message(chat_id, log, reply_to_message_id=message.message_id)
         else:
             bot.send_photo(chat_id, img_byte_new, reply_to_message_id=message.message_id)
         
-        
     
 ## Configure the webhook for the bot with the url of the Glitch project
+
 bot.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'], environ['TELEGRAM_TOKEN']))
+
--- src/imgProcessor.py
+++ src/imgProcessor.py
@@ -14,7 +14,7 @@ from PIL import ImageOps
 
 ## Processor commands (options)
 
-proc_cmds = ['/neg', '/ctr', '/bgt', '/sat', '/shp', '/w2x']
+proc_cmds = ['/neg', '/ctr', '/bgt', '/sat', '/shp', '/w2x', '/col', '/cap', '/drm']
 
 
 ## Return the processor commands
@@ -28,7 +28,7 @@ def get_proc_cmds():
 def process_byte(img_byte, cmd_name, cmd_args=[]):
     img_stream = io.BytesIO(img_byte)
     img = Image.open(img_stream)
-    errors = False
+    log_only = False
     log = ''
     
     if cmd_name == '/neg':
@@ -36,13 +36,13 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
         
     elif cmd_name == '/ctr':
         if not len(cmd_args) == 1:
-            errors = True
+            log_only = True
         else:
             try:
                 cmd_arg = float(cmd_args[0])
             except ValueError: 
-                errors = True
-        if not errors:
+                log_only = True
+        if not log_only:
             cmd_arg = float(cmd_args[0])
             img_new = proc_ctr(img, cmd_arg)
             img_stream = io.BytesIO()
@@ -54,13 +54,13 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
     
     elif cmd_name == '/bgt':
         if not len(cmd_args) == 1:
-            errors = True
+            log_only = True
         else:
             try:
                 cmd_arg = float(cmd_args[0])
             except ValueError: 
-                errors = True
-        if not errors:
+                log_only = True
+        if not log_only:
             cmd_arg = float(cmd_args[0])
             img_new = proc_bgt(img, cmd_arg)
             img_stream = io.BytesIO()
@@ -72,13 +72,13 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
     
     elif cmd_name == '/sat':
         if not len(cmd_args) == 1:
-            errors = True
+            log_only = True
         else:
             try:
                 cmd_arg = float(cmd_args[0])
             except ValueError: 
-                errors = True
-        if not errors:
+                log_only = True
+        if not log_only:
             cmd_arg = float(cmd_args[0])
             img_new = proc_sat(img, cmd_arg)
             img_stream = io.BytesIO()
@@ -90,13 +90,13 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
     
     elif cmd_name == '/shp':
         if not len(cmd_args) == 1:
-            errors = True
+            log_only = True
         else:
             try:
                 cmd_arg = float(cmd_args[0])
             except ValueError: 
-                errors = True
-        if not errors:
+                log_only = True
+        if not log_only:
             cmd_arg = float(cmd_args[0])
             img_new = proc_shp(img, cmd_arg)
             img_stream = io.BytesIO()
@@ -107,13 +107,19 @@ def process_byte(img_byte, cmd_name, cmd_args=[]):
             log = 'Invalid arguments.'
     
     elif cmd_name == '/w2x':
-        img_stream = img2x(img_byte)
-    
-    else:
-        img_new = img
-    
-    
-    return img_stream, errors, log
+        img_stream = img_2x(img_byte)
+        
+    elif cmd_name == '/col':
+        img_stream = img_col(img_byte)
+        
+    elif cmd_name == '/cap':
+        img_stream, log = img_cap(img_byte)
+        log_only = True
+        
+    elif cmd_name == '/drm':
+        img_stream = img_drm(img_byte)
+        
+    return img_stream, log_only, log
   
 
 ## Processors
@@ -144,10 +150,10 @@ def proc_shp(img, factor):
     return img
   
 
-## Enlarger
+## AI processing
 
 # enlarge an image (double its size) using the Waifu2x API
-def img2x(img_byte):
+def img_2x(img_byte):
     img_stream = io.BytesIO(img_byte)
     r = requests.post(
         "https://api.deepai.org/api/waifu2x",
@@ -156,7 +162,46 @@ def img2x(img_byte):
         },
         headers={'api-key': environ['DEEPAI_API_KEY']}
     )
-    print(r.json())
+    img_new = requests.get(r.json().get('output_url')).content
+    return img_new
+
+# colorize an image using another deepai API
+def img_col(img_byte):
+    img_stream = io.BytesIO(img_byte)
+    r = requests.post(
+        "https://api.deepai.org/api/colorizer",
+        files={
+            'image': img_byte,
+        },
+        headers={'api-key': environ['DEEPAI_API_KEY']}
+    )
+    img_new = requests.get(r.json().get('output_url')).content
+    return img_new
+
+# generate image captions (silly ones, indeed)
+def img_cap(img_byte):
+    img_stream = io.BytesIO(img_byte)
+    r = requests.post(
+        "https://api.deepai.org/api/neuraltalk",
+        files={
+            'image': img_byte,
+        },
+        headers={'api-key': environ['DEEPAI_API_KEY']}
+    )
+    gen_caption = r.json().get('output')
+    print(gen_caption)
+    return img_stream, gen_caption
+
+# classical deep dream
+def img_drm(img_byte):
+    img_stream = io.BytesIO(img_byte)
+    r = requests.post(
+        "https://api.deepai.org/api/deepdream",
+        files={
+            'image': img_byte,
+        },
+        headers={'api-key': environ['DEEPAI_API_KEY']}
+    )
     img_new = requests.get(r.json().get('output_url')).content
     return img_new
 
